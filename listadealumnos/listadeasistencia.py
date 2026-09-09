5d1882b7475af37b15cabe6a38d8284be34456f3
GordoBondiola789 <colchiciniemanuel@gmail.com>
2026-09-09 15:59:18 -0300

    Delete listadealumnos directory

--- listadealumnos/listadeasistencia.py
+++
@@ -1,9 +0,0 @@
-alumnos = []  # Creamos una lista vacía para guardar los nombres
-
-nombre1 = input("Ingrese el nombre del primer alumno: ")  # Pedimos el primer nombre
-alumnos.append(nombre1)  # Agregamos el primer nombre a la lista
-
-nombre2 = input("Ingrese el nombre del segundo alumno: ")  # Pedimos el segundo nombre
-alumnos.append(nombre2)  # Agregamos el segundo nombre a la lista
-
-print(alumnos)  # Mostramos todos los alumnos guardados
