5e16b37863b1b20f6d6082b971d82a0f0aeaf474
GordoBondiola789 <colchiciniemanuel@gmail.com>
2026-09-09 16:00:00 -0300

    Add files via upload

---
+++ CRUD-TP1/perfildealumno.py
@@ -0,0 +1,11 @@
+alumno = {}  # Creamos un diccionario vacío
+
+nombre = input("Ingrese el nombre: ")  # Pedimos el nombre del alumno
+edad = input("Ingrese la edad: ")  # Pedimos la edad del alumno
+curso = input("Ingrese el curso: ")  # Pedimos el curso del alumno
+
+alumno["nombre"] = nombre  # Guardamos el nombre en el diccionario
+alumno["edad"] = edad  # Guardamos la edad en el diccionario
+alumno["curso"] = curso  # Guardamos el curso en el diccionario
+
+print(alumno)  # Mostramos toda la información del alumno
